9fd946ed67851e70333d4c35519eb797b2841174
MatthiasBenaets <matthias.benaets@gmail.com>
2024-10-26 19:11:21 +0200

    feat: basemap selector

--- src/lib/components/Map.svelte
+++ src/lib/components/Map.svelte
@@ -1,14 +1,17 @@
 <script lang="ts">
 	import { MapLibre } from 'svelte-maplibre';
+	import MapStyle from './MapStyle.svelte';
+	import Tiles from '$lib/data/mapTiles.json';
+	import type { StyleSpecification } from '@maplibre/maplibre-gl-style-spec';
+
+	let style: StyleSpecification = $state(
+		(Tiles as unknown as Record<string, StyleSpecification>).OpenStreetMap
+	);
 </script>
 
-<MapLibre
-	center={[50, 20]}
-	zoom={7}
-	class="map"
-	standardControls
-	style="https://basemaps.cartocdn.com/gl/positron-gl-style/style.json"
-/>
+<MapLibre center={[50, 20]} zoom={7} class="map" standardControls {style}>
+	<MapStyle bind:style />
+</MapLibre>
 
 <style>
 	:global(.map) {
---
+++ src/lib/components/MapStyle.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import { Control, ControlGroup } from 'svelte-maplibre';
+	import type { StyleSpecification } from '@maplibre/maplibre-gl-style-spec';
+	import Tiles from '$lib/data/mapTiles.json';
+
+	let { style = $bindable() }: { style: StyleSpecification } = $props();
+</script>
+
+<Control class="flex w-full flex-col gap-y-2">
+	<ControlGroup>
+		<select bind:value={style} class="w-full bg-transparent p-1 px-2">
+			{#each Object.entries(Tiles) as [key, test]}
+				<option value={test}>{key}</option>
+			{/each}
+		</select>
+	</ControlGroup>
+</Control>
+
+<style>
+	:global(.maplibregl-ctrl-group button) {
+		width: 100%;
+	}
+</style>
